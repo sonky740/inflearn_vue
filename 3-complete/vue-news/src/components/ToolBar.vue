93b91f48aa3e3ec7cf6ab4425de6ba1295b35888
Sonky <sonky740@gmail.com>
2022-08-27 23:59:09 +0900

    3-complete - axios를 이용한 api 호출 ~~

---
+++ 3-complete/vue-news/src/components/ToolBar.vue
@@ -0,0 +1,38 @@
+<template>
+  <header class="header">
+    <router-link to="/news">News</router-link>
+    <router-link to="/ask">Ask</router-link>
+    <router-link to="/jobs">Jobs</router-link>
+  </header>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style scoped>
+.header {
+  display: flex;
+  justify-content: center;
+  padding: 8px;
+  background: #42b883;
+  color: #fff;
+}
+
+.header a {
+  position: relative;
+  color: inherit;
+} 
+
+.header a + a {
+  margin-left: 20px;
+}
+
+.header .router-link-exact-active {
+  color: #354953;
+}
+</style>
---
+++ 3-complete/vue-news/src/routes/index.js
@@ -0,0 +1,39 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import AskView from '../views/AskView.vue';
+import JobsView from '../views/JobsView.vue';
+import NewsView from '../views/NewsView.vue';
+import ItemView from '../views/ItemView.vue';
+import UserView from '../views/UserView.vue';
+
+Vue.use(VueRouter);
+
+export const router = new VueRouter({
+  mode: 'history',
+  routes: [
+    {
+      path: '/',
+      redirect: '/news',
+    },
+    {
+      path: '/news',
+      component: NewsView,
+    },
+    {
+      path: '/ask',
+      component: AskView,
+    },
+    {
+      path: '/jobs',
+      component: JobsView,
+    },
+    {
+      path: '/item',
+      component: ItemView,
+    },
+    {
+      path: '/user',
+      component: UserView,
+    },
+  ],
+});
