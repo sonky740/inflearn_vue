4a60a0e3ec5c9b4f6d22ce0f0436bba766ed8224
손기연 <sonky740@gmail.com>
2022-08-30 16:39:02 +0900

    3-complete - Vue.js 플러그인 소개 및 차트 플러그인 제작 ~~

---
+++ 3-complete/vue-charts/src/components/LineChart.vue
@@ -0,0 +1,29 @@
+<template>
+  <canvas ref="myChart"></canvas>
+</template>
+
+<script>
+import Chart from 'chart.js';
+
+export default {
+  mounted() {
+    new Chart(this.$refs.myChart, {
+      type: 'line',
+      data: {
+        labels: ['January', 'February', 'March', 'April', 'May', 'June'],
+        datasets: [
+          {
+            label: 'My First dataset',
+            backgroundColor: 'rgb(255, 99, 132)',
+            borderColor: 'rgb(255, 99, 132)',
+            data: [0, 10, 5, 2, 20, 30, 45],
+          },
+        ],
+      },
+      options: {},
+    });
+  },
+};
+</script>
+
+<style></style>
