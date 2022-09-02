77eeace3b43e43d0f53a7ec03819b4c7488c4f1a
Sonky <sonky740@gmail.com>
2022-09-03 00:14:56 +0900

    4-end - complete

--- 4-end/vue-til/src/components/LoginForm.spec.js
+++ 4-end/vue-til/src/components/LoginForm.spec.js
@@ -1,8 +1,31 @@
-import { sum } from './math';
+import { shallowMount } from '@vue/test-utils';
+import LoginForm from './LoginForm.vue';
 
-describe('math.js', () => {
-  test('10 + 20 = 30', () => {
-    const result = sum(10, 20);
-    expect(result).toBe(30);
+describe('LoginForm.vue', () => {
+  test('ID가 이메일 형식이 아니면 경고 메시지가 출력된다.', () => {
+    const wrapper = shallowMount(LoginForm, {
+      data() {
+        return {
+          username: 'eqwwr',
+        };
+      },
+    });
+    const warningText = wrapper.find('.warning');
+    console.log(warningText.html());
+    expect(warningText.exists()).toBeTruthy();
+  });
+
+  test('ID와 PW가 입력되지 않으면 로그인 버튼이 비활성화 된다.', () => {
+    const wrapper = shallowMount(LoginForm, {
+      data() {
+        return {
+          username: '',
+          password: '',
+        };
+      },
+    });
+
+    const button = wrapper.find('button');
+    expect(button.element.disabled).toBeTruthy();
   });
 });
