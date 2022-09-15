c3b61d0195458752f5b70b98d80e8b85cfa76b17
Sonky <sonky740@gmail.com>
2022-09-16 00:53:22 +0900

    6-nuxt - JSON Server 소개 ~~

---
+++ 6-nuxt/learn-nuxt/layouts/error.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <p>페이지를 찾을 수 없습니다.</p>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ 6-nuxt/learn-nuxt/pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <p>넉스트 시작하기</p>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped></style>
