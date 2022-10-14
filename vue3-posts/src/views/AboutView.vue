31635404b4b010e77fffaa5920a3262d16031ff9
Sonky <sonky740@gmail.com>
2022-10-15 01:20:44 +0900

    vue3 posts start

---
+++ vue3-posts/src/views/AboutView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h2>About View</h2>
+    <p>{{ $route.path }}</p>
+    <p>{{ $route.name }}</p>
+    <button type="button" class="btn btn-primary" @click="$router.push('/')">
+      Home 이동
+    </button>
+  </div>
+</template>
+
+<script setup>
+import { useRoute } from 'vue-router';
+
+const route = useRoute();
+console.log('route.path: ', route.path);
+</script>
+
+<style lang="scss" scoped></style>
