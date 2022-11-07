fbfdb509ee0118d663568c9a6cb32193f0feaad3
Sonky <sonky740@gmail.com>
2022-11-08 00:29:00 +0900

    115~~

---
+++ udemy/109-components/src/components/UserInfo.vue
@@ -0,0 +1,34 @@
+<template>
+  <section>
+    <BaseCard>
+      <template #header>
+        <h3>{{ fullName }}</h3>
+        <BaseBadge :type="role" :caption="role.toUpperCase()"></BaseBadge>
+      </template>
+      <template #default>
+        <p>{{ infoText }}</p>
+      </template>
+    </BaseCard>
+  </section>
+</template>
+
+<script>
+import BaseBadge from './BaseBadge.vue';
+import BaseCard from './BaseCard.vue';
+
+export default {
+  components: {
+    BaseBadge,
+    BaseCard,
+  },
+  props: ['fullName', 'infoText', 'role'],
+};
+</script>
+
+<style scoped>
+/* section header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+} */
+</style>
